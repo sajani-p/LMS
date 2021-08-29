b92aa621b09bb4446fb09ff9b298a42b33b9fed0
sajani-p <sajipathegama@gmail.com>
2021-08-29 06:39:39 +0530

    testing styled component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import styled from "styled-components/";
+
+const Title = styled.h1`
+  font-size: 3em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 function App() {
   return (
-    <h1 style={{textAlign: "center"}}>It's gonna be awesome!</h1>
+    <Wrapper>
+      <Title>
+        It's gonna be awesome!
+      </Title>
+    </Wrapper>
   );
 }
 
