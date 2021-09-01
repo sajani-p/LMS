eb96b34e8149eefec689f5346e76acf0146c5836
sajani-p <sajipathegama@gmail.com>
2021-09-01 10:56:33 +0530

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import {Header, Main, Footer} from "./components/Layout";
+import { NavBar, NavItem,NavLink } from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Header>This is the header</Header>
+      <Header>
+        <NavBar>
+          <NavItem href="#">
+            <NavLink>Catalog</NavLink>
+          </NavItem>
+          <NavItem href="#">
+            <NavLink>DashBoard</NavLink>
+          </NavItem>
+        </NavBar>
+
+      </Header>
       <Main>This is the main</Main>
       <Footer>This is the footer</Footer>
     </>
