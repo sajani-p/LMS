e580033c539f1117bf53675d78c668fa02acfb81
sajani-p <sajipathegama@gmail.com>
2021-09-04 07:13:39 +0530

    add theming support and make app bar fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import {ThemeProvider} from "styled-components"
+
 import {Header, Main, Footer} from "./components/Layout";
 import { NavBar, NavItem,NavLink } from "./components/Navbar";
 
 function App() {
+  const theme = {
+    primary:{
+      main: "#29b6f6",
+      light: "#73e8ff",
+      dark: "#0086c3",
+      textColor: "#000",
+    },
+    secondary: {
+      main: "#fff",
+    },
+  };
+
   return (
-    <>
+    <ThemeProvider theme = {theme}>
       <Header>
         <NavBar>
           <NavItem href="#">
@@ -18,7 +32,7 @@ function App() {
       </Header>
       <Main>This is the main</Main>
       <Footer>This is the footer</Footer>
-    </>
+    </ThemeProvider>
   );
 }
 
