f3728cee8fac6f32d4b1c0b061d019528cbb726e
sajani-p <sajipathegama@gmail.com>
2021-10-21 19:32:53 +0530

    Populate options with API in LendDialog

---
+++ src/api/memberAPI.js
@@ -0,0 +1,5 @@
+export const getMembers = () => [
+    { id: "1", name: "Sajani Pathegama"},
+    { id: "2", name: "Imalsha Ranepura"}
+];
+//promise ekak wage return wenn dalath hadnna
--- src/containers/Dashboard/Books/LendDialog.js
+++ src/containers/Dashboard/Books/LendDialog.js
@@ -1,38 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Button, FlexRow, Select } from "../../../components/CommonComponents";
 import { Modal, DialogBox } from "../../../components/Modal";
 
-export default function LendDialog ({ handleClose, show }) {
-    const [member, setMember] = useState("");
+import Spinner from "../../../components/Spinner";
+import { getMembers } from "../../../api/memberAPI";
 
-    const sendConfirm = () => handleClose(true, member);  //confirmation ek true da kyl check krl true nm memberta msg eka pass wenna one. e nisa member pass wenna one methana
+export default function LendDialog({ handleClose, show }) {
+  const [member, setMember] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [members, setMembers] = useState(null);
 
-    const sendCancel = () => handleClose(false, null);
+  const sendConfirm = () => {
+    if (member !== "") {
+      handleClose(true, member);
+    } else {
+      window.alert("Please select a member first.");
+    }
+  };
 
-    return (
-        <Modal show={show}>
-            <DialogBox>
-                <h2>Lend Book</h2>
-                <p>Select member to continue and confirm</p>
-                <Select
-                    id="member-select"
-                    onChange={(e) => setMember(e.target.value)}
-                    value={member}
-                >
-                    <option value="">--Please choose an option--</option>
-                    <option value="dog">Dog</option>
-                    <option value="cat">Cat</option>
-                    <option value="hamaster">Hamaster</option>
-                    <option value="parrot">Parrot</option>
-                    <option value="spider">Spider</option>
-                    <option value="goldfish">GoldFish</option>
-                </Select>
-                <FlexRow>
-                    <Button onClick={sendConfirm}>Confirm</Button>
-                    <Button onClick={sendCancel} color="secondary">Cancel</Button>
-                </FlexRow>
-            </DialogBox>
-        </Modal>
-    );
+  const sendCancel = () => handleClose(false, null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const response = getMembers();
+    setMembers(response);
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <Modal show={show}>
+      <DialogBox>
+        <h2>Lend Book</h2>
+        <p>Select member to continue and confirm</p>
+        {!isLoading && members !== null ? (
+          <>
+            <Select
+              id="member-select"
+              onChange={(e) => setMember(e.target.value)}
+              value={member}
+            >
+              <option value="">--Please select a member--</option>
+              {members.map((member, index) => (
+                <option key={index} value={member.id}>
+                  {member.name}
+                </option>
+              ))}
+            </Select>
+            <FlexRow>
+              <Button onClick={sendConfirm}>Confirm</Button>
+              <Button onClick={sendCancel} color="secondary">
+                Cancel
+              </Button>
+            </FlexRow>
+          </>
+        ) : (
+          <Spinner />
+        )}
+      </DialogBox>
+    </Modal>
+  );
 }
